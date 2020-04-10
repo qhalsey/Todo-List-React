5b0b313c5f0e09cacd8e204178e029aa1c849941
Quentin Halsey <quentin.developer@outlook.com>
2020-04-09 17:03:32 -0700

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDweFZ9mdi9w6LV_nschjasDNXK1EhBgMo",
+  authDomain: "todo-list-2dddc.firebaseapp.com",
+  databaseURL: "https://todo-list-2dddc.firebaseio.com",
+  projectId: "todo-list-2dddc",
+  storageBucket: "todo-list-2dddc.appspot.com",
+  messagingSenderId: "611830660414",
+  appId: "1:611830660414:web:dfb44a178dec21e93f806f",
+  measurementId: "G-JZ5DY84KE9",
+};
+
+export { firebaseConfig as firebase };
